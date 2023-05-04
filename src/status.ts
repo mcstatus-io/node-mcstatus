be40db4c3f30532e5c9d509dd922d0a99f79deea
Hitesh. V <91016099+hvlxh@users.noreply.github.com>
2023-05-04 23:10:58 +0530

    added not added options in JavaStatusResponse
    `plugins`, and `software` were missing so i added them.

--- src/status.ts
+++ src/status.ts
@@ -1,81 +1,103 @@
-import superagent from 'superagent';
-import { BASE_URL } from './enum';
+import superagent from 'superagent'
+import { BASE_URL } from './enum'
 
 interface StatusResponse {
-	online: boolean,
-	host: string,
-	port: number,
-	eula_blocked: boolean,
-	retrieved_at: number,
-	expires_at: number
+  online: boolean
+  host: string
+  port: number
+  eula_blocked: boolean
+  retrieved_at: number
+  expires_at: number
 }
 
 interface JavaStatusResponse extends StatusResponse {
-	version?: {
-		name_raw: string,
-		name_clean: string,
-		name_html: string,
-		protocol: number
-	} | null,
-	players?: {
-		online: number,
-		max: number,
-		list: {
-			uuid: string,
-			name_raw: string,
-			name_clean: string,
-			name_html: string
-		}[]
-	},
-	motd?: {
-		raw: string,
-		clean: string,
-		html: string
-	},
-	icon?: string | null,
-	mods?: {
-		name: string,
-		version: string
-	}[]
+  version?: {
+    name_raw: string
+    name_clean: string
+    name_html: string
+    protocol: number
+  } | null
+  players?: {
+    online: number
+    max: number
+    list: {
+      uuid: string
+      name_raw: string
+      name_clean: string
+      name_html: string
+    }[]
+  }
+  motd?: {
+    raw: string
+    clean: string
+    html: string
+  }
+  icon?: string | null
+  mods?: {
+    name: string
+    version: string
+  }[]
+  plugins?: {
+    name: string
+    version: string
+  }[]
+  software?: string
 }
 
 interface BedrockStatusResponse extends StatusResponse {
-	version?: {
-		name: string,
-		protocol: number
-	},
-	players?: {
-		online: number,
-		max: number
-	},
-	motd?: {
-		raw: string,
-		clean: string,
-		html: string
-	},
-	gamemode?: string,
-	server_id?: string,
-	edition?: 'MCPE' | 'MCEE'
+  version?: {
+    name: string
+    protocol: number
+  }
+  players?: {
+    online: number
+    max: number
+  }
+  motd?: {
+    raw: string
+    clean: string
+    html: string
+  }
+  gamemode?: string
+  server_id?: string
+  edition?: 'MCPE' | 'MCEE'
 }
 
-const statusJava = async (host: string, port = 25565): Promise<JavaStatusResponse> => {
-	const result = await superagent.get(`${BASE_URL}/status/java/${host}:${port}`);
+const statusJava = async (
+  host: string,
+  port = 25565
+): Promise<JavaStatusResponse> => {
+  const result = await superagent.get(`${BASE_URL}/status/java/${host}:${port}`)
 
-	if (result.statusCode !== 200) {
-		throw new Error(result.body);
-	}
+  if (result.statusCode !== 200) {
+    throw new Error({
+      ...result.body,
+      status: result.statusCode
+    })
+  }
 
-	return result.body as JavaStatusResponse;
-};
+  return result.body as JavaStatusResponse
+}
 
-const statusBedrock = async (host: string, port = 19132): Promise<BedrockStatusResponse> => {
-	const result = await superagent.get(`${BASE_URL}/status/bedrock/${host}:${port}`);
+const statusBedrock = async (
+  host: string,
+  port = 19132
+): Promise<BedrockStatusResponse> => {
+  const result = await superagent.get(
+    `${BASE_URL}/status/bedrock/${host}:${port}`
+  )
 
-	if (result.statusCode !== 200) {
-		throw new Error(result.body);
-	}
+  if (result.statusCode !== 200) {
+    throw new Error(result.body)
+  }
 
-	return result.body as BedrockStatusResponse;
-};
+  return result.body as BedrockStatusResponse
+}
 
-export { statusJava, statusBedrock, StatusResponse, JavaStatusResponse, BedrockStatusResponse };
+export {
+  statusJava,
+  statusBedrock,
+  StatusResponse,
+  JavaStatusResponse,
+  BedrockStatusResponse
+}
